d7cdc03c0881f50b34f11f83b32e0fcd2631cdb9
Bruno <bwprado@gmail.com>
2025-10-14 23:18:18 -0300

    Refactor web scraping logic to support multiple categories and improve product data extraction

--- main.ts
+++ main.ts
@@ -3,127 +3,78 @@
 import * as cheerio from 'npm:cheerio'
 import puppeteer from 'npm:puppeteer'
 
-async function test() {
-  const store = 'https://www.kabum.com.br'
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(
-    `${store}/hardware/processadores?page_number=1&page_size=1000&facet_filters=&sort=most_searched`,
-    {
-      waitUntil: 'networkidle2'
-    }
-  )
-
-  const products = await page.evaluate(() => {
-    const store = 'https://www.kabum.com.br'
-    return Array.from(document.querySelectorAll('article.productCard')).map(
-      (card: Element) => {
-        const name = card.querySelector('.nameCard')?.textContent?.trim() || ''
-        const price =
-          card.querySelector('.priceCard')?.textContent?.trim() || ''
-        const oldPrice =
-          card.querySelector('.oldPriceCard')?.textContent?.trim() || ''
-        const tag =
-          card
-            .querySelector(
-              '[class*="flex"][class*="items-center"][class*="relative"][class*="h-[20px]"]'
-            )
-            ?.textContent?.trim() || ''
-        const image =
-          card.querySelector('.imageCard')?.getAttribute('src') || ''
-        const link = `${store}${
-          card.querySelector('a.productLink')?.getAttribute('href') || ''
-        }`
-        return { name, price, oldPrice, tag, image, link }
-      }
-    )
-  })
-
-  console.log(products[0])
-  console.log(products.length)
-  await browser.close()
+interface Product {
+  name: string
+  price: string
+  oldPrice: string
+  tag: string
+  image: string
+  link: string
+  category: string
 }
 
-async function main() {
-  try {
-    const storeUrl = 'https://www.kabum.com.br/hardware/processadores'
-    const response = await fetch(storeUrl, {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-      }
-    })
-    const html = await response.text()
-    Deno.writeTextFileSync('html.txt', html)
-    console.log(html.includes('productCard'))
-    const $ = cheerio.load(html)
-    const products = $('article .productCard')
-    console.log(products.length)
-    products.each((index, element) => {
-      console.log(index)
-      const product = $(element)
-      console.log(product.text())
-      const name = product.find('.nameCard').text().trim()
-      const price = product.find('.priceCard').text().trim()
-      console.log(`${index + 1}. ${name} - ${price}`)
-    })
-  } catch (error) {
-    console.error(error)
-  }
+const WEBSITES = [
+  ['processadores', 'https://www.kabum.com.br/hardware/processadores'],
+  ['placas-de-video', 'https://www.kabum.com.br/hardware/placas-de-video'],
+  ['placas-mae', 'https://www.kabum.com.br/hardware/placas-mae'],
+  ['memorias', 'https://www.kabum.com.br/hardware/memorias'],
+  ['armazenamento', 'https://www.kabum.com.br/hardware/armazenamento'],
+  ['fontes', 'https://www.kabum.com.br/hardware/fontes'],
+  ['coolers', 'https://www.kabum.com.br/hardware/coolers'],
+  ['gabinetes', 'https://www.kabum.com.br/hardware/gabinetes'],
+  ['perifericos', 'https://www.kabum.com.br/hardware/perifericos'],
+  ['placas-de-video', 'https://www.kabum.com.br/hardware/placas-de-video']
+]
+
+const getTextContent = (element: Element, selector: string) => {
+  return element.querySelector(selector)?.textContent?.trim() || ''
 }
 
 async function scrapeWithPagination() {
-  const store = 'https://www.kabum.com.br'
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  let allProducts = []
+  let allProducts: any[] = []
   let pageNumber = 1
   let hasNextPage = true
 
-  while (hasNextPage && pageNumber <= 5) {
-    // Limit to 5 pages for safety
-    console.log(`Scraping page ${pageNumber}...`)
-    await page.goto(
-      `${store}/hardware/processadores?page_number=${pageNumber}&page_size=20`,
-      { waitUntil: 'networkidle2' }
-    )
-
-    const products = await page.evaluate((baseUrl) => {
-      return Array.from(document.querySelectorAll('article.productCard')).map(
-        (card: Element) => {
-          const name =
-            card.querySelector('.nameCard')?.textContent?.trim() || ''
-          const price =
-            card.querySelector('.priceCard')?.textContent?.trim() || ''
-          const oldPrice =
-            card.querySelector('.oldPriceCard')?.textContent?.trim() || ''
-          const tag =
-            card
-              .querySelector(
-                '[class*="flex"][class*="items-center"][class*="relative"][class*="h-[20px]"]'
-              )
-              ?.textContent?.trim() || ''
-          const image =
-            card.querySelector('.imageCard')?.getAttribute('src') || ''
-          const link = `${baseUrl}${
-            card.querySelector('a.productLink')?.getAttribute('href') || ''
-          }`
-          return { name, price, oldPrice, tag, image, link }
-        }
+  for (const [category, url] of WEBSITES) {
+    while (hasNextPage && pageNumber <= 5) {
+      // Limit to 5 pages for safety
+      console.log(`Scraping page ${pageNumber} of ${category}, on ${url}...`)
+      await page.goto(
+        `${url}?page_number=${pageNumber}&page_size=20&page_size=100`,
+        { waitUntil: 'networkidle2' }
       )
-    }, store)
 
-    allProducts = [...allProducts, ...products]
+      const products = await page.evaluate((baseUrl) => {
+        return Array.from(document.querySelectorAll('article.productCard')).map(
+          (card: Element) => {
+            const name = getTextContent(card, '.nameCard')
+            const price = getTextContent(card, '.priceCard')
+            const oldPrice = getTextContent(card, '.oldPriceCard')
+            const tag = getTextContent(
+              card,
+              '[class*="flex"][class*="items-center"][class*="relative"][class*="h-[20px]"]'
+            )
+            const image = getTextContent(card, '.imageCard')
+            const link = getTextContent(card, 'a.productLink')
+            return { name, price, oldPrice, tag, image, link }
+          }
+        )
+      }, url)
 
-    // Check if there's a next page button that's not disabled
-    const hasNext = await page.evaluate(() => {
-      const nextButton = document.querySelector('[aria-label="Next page"]')
-      return nextButton && !nextButton.hasAttribute('disabled')
-    })
+      allProducts = [...allProducts, ...products]
 
-    hasNextPage = hasNext
-    pageNumber++
+      // Check if there's a next page button that's not disabled
+      const hasNext = await page.evaluate(() => {
+        const nextButton = document.querySelector('[aria-label="Next page"]')
+        return nextButton && !nextButton.hasAttribute('disabled')
+      })
+
+      hasNextPage = hasNext
+      pageNumber++
+    }
   }
 
   console.log(`Total products scraped: ${allProducts.length}`)
